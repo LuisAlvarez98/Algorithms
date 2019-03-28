402b8bca81a3bf4c9347b6ec950e30569a7e40b2
Luis Felipe Alvarez <luisfeal1998@hotmail.com>
2019-03-28 12:13:49 -0600

    Dijkstra

--- Quick Sort Algorithm/QuickSort.cpp
+++ Quick Sort Algorithm/QuickSort.cpp
@@ -4,50 +4,18 @@
 
 using namespace std;
 
-int contComp = 0;
-
 //&function
 
-void Particion(vector<int> &d, int ini, int fin, int &pivote) {
-	int elemPivote = d[ini];
-	int j = ini;
-	for (int i = ini + 1; i <= fin; i++) {
-		contComp++;
-		if (d[i] < elemPivote) {
-			j++;
-			int aux = d[i];
-			d[i] = d[j];
-			d[j] = aux;
+i moveZeroes(vector<int>& nums) {
+	for (int i = 0; i < nums.size(); i++) {
+		if (nums[i] < nums[i + 1]) {
+			cout << nums[i];
 		}
 	}
-	pivote = j;
-	int aux = d[ini];
-	d[ini] = d[pivote];
-	d[pivote] = d[ini];
 }
-
-void QuickSort(vector<int> &d, int ini, int fin) {
-	int pivote = ini;
-	if (ini < fin) {
-		Particion(d, ini, fin, pivote);
-		QuickSort(d,ini, pivote - 1);
-		QuickSort(d, pivote + 1, fin);
-	}
-}
-
 int main() {
-	int n;
-	cin >> n;
-	vector<int> datos(n);
-	for (int i = 0; i<n; i++) {
-		cin >> datos[i];
-	}
-	QuickSort(datos, 0, n - 1);
-	cout << contComp << endl;
-	/*
-	for (int i=0; i<n; i++){
-	cout<<datos[i]<< " ";
-	}
-	cout << endl;
-	*/
+	vector<int>nums = { 3,4,5,1,2 };
+	vector<int>res;
+	res = moveZeroes(nums);
+
 }
